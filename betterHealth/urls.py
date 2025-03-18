a14aa1bdfac982cde597efc0fbd2a15b79e072a7
Diogo Filipe Alves Pires <dfalvesp@gmail.com>
2025-03-18 18:37:36 +0100

    html start

--- betterHealth/urls.py
+++ betterHealth/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from healthApp.views import landing
+from healthApp.views import landing, register, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', landing),
+    path('register/', register),
+    path('home/', home),
 ]
--- healthApp/views.py
+++ healthApp/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
-
-# Create your views here.
 def landing(request):
-    return render(request, 'landing.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to a home page or any other page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'landing.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('landing')  # Redirect to the login page after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def home(request):
+    return render(request, 'home.html')
