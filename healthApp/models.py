4ea976899a943281e90cfe442b7b6c7a5e6dec60
Oriol-Farras <158330943+Oriol-Farras@users.noreply.github.com>
2025-03-18 17:47:47 +0100

    feat(database): create initial database models

--- healthApp/models.py
+++ healthApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    dni = models.CharField(max_length=9, unique=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    has_insurance = models.BooleanField(default=False)
+    insurance_number = models.CharField(max_length=50, blank=True, null=True)  # Only if the patient has insurance
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({'Insured' if self.has_insurance else 'Uninsured'})"
+
