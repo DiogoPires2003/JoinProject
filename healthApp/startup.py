942d8a3817ba5fbeabd29830efa65d05dc661bd1
Oriol-Farras <158330943+Oriol-Farras@users.noreply.github.com>
2025-04-17 14:14:19 +0200

    feat(startip): email and password in enviroment variables

--- healthApp/startup.py
+++ healthApp/startup.py
@@ -1,3 +1,10 @@
+import os
+from django.contrib.auth.hashers import make_password
+from .models import Employee, Role
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def create_default_roles():
     from .models import Role
     from django.db.utils import OperationalError, ProgrammingError
@@ -11,26 +18,24 @@ def create_default_roles():
     except (OperationalError, ProgrammingError):
         pass
 
+
 def create_super_employee():
-    from .models import Employee, Role
-    from django.contrib.auth.hashers import make_password
-    from django.db.utils import OperationalError, ProgrammingError
+    administrator_email = os.environ.get("ADMINISTRATOR_EMAIL")
+    administrator_password = os.environ.get("ADMINISTRATOR_PASSWORD")
 
-    try:
-        administrator_role, _ = Role.objects.get_or_create(
-            name="Administrator",
-            defaults={"description": "Role with full access to all administrator functionalities."}
-        )
+    # Asegúrate de que el rol "Administrator" existe
+    administrator_role, _ = Role.objects.get_or_create(
+        name="Administrator",
+        defaults={"description": "Administrador del sistema"}
+    )
 
-        Employee.objects.get_or_create(
-            email="administrator@betterhealth.com",
-            defaults={
-                "first_name": "Super",
-                "last_name": "Administrator",
-                "role": administrator_role,
-                "password": make_password("admin1234")  # Hashea la contraseña
-            }
-        )
-    except (OperationalError, ProgrammingError):
-        pass
-
+    # Crea el empleado administrador si no existe
+    Employee.objects.get_or_create(
+        email=administrator_email,
+        defaults={
+            "first_name": "Super",
+            "last_name": "Administrator",
+            "role": administrator_role,
+            "password": make_password(administrator_password)
+        }
+    )
