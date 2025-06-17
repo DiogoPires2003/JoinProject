84bb3d7b15e72790387ff28bece5e3cd40d7f20f
Diogo Filipe Alves Pires <dfalvesp@gmail.com>
2025-06-17 13:59:29 +0200

    Update decorators.py

--- healthApp/decorators.py
+++ healthApp/decorators.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect
 from functools import wraps
+from django.http import HttpResponseForbidden
+
 
 def admin_required(view_func):
     @wraps(view_func)
@@ -14,5 +16,13 @@ def redirect_admin(view_func):
     def wrapper(request, *args, **kwargs):
         if request.session.get('is_admin'):
             return redirect('admin_area')
+        return view_func(request, *args, **kwargs)
+    return wrapper
+
+
+def financer_required(view_func):
+    def wrapper(request, *args, **kwargs):
+        if not request.user.groups.filter(name='Financer').exists():
+            return HttpResponseForbidden("You do not have permission to access this page.")
         return view_func(request, *args, **kwargs)
     return wrapper
